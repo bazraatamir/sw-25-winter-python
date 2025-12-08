c4fa1547734e01c71ec7f99db594d70b5efe3128
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2025-12-08 09:55:10 +0800

    hangman commit

--- lab-1/hangman.py
+++ lab-1/hangman.py
@@ -2,24 +2,45 @@
 import random
 
 def choose_word(word_list):
-    
     # word_list-ээс нэг үгийг random буюу санамсаргүйгээр сонгоно
+    word = random.choice(word_list)
     # Сонгосон үгийг бүх том үсэг болгон буцаана
+    return word.upper()
+   
+   
     pass
 
 def display_progress(word, guessed_letters):
-    # Нууц үгийн бүх үсгийг нэг нэгээр шалгана
-    # Хэрэв тоглогч таасан бол тухайн үсгийг харуулна
-    # Хэрэв таагаагүй бол '_' гэж харуулна
+    progress = ""
+    for letter in word:
+         # Нууц үгийн бүх үсгийг нэг нэгээр шалгана
+        if letter in guessed_letters:
+             # Хэрэв тоглогч таасан бол тухайн үсгийг харуулна
+            progress += letter + "*"
+        else:
+            # Хэрэв таагаагүй бол '*' гэж харуулна
+            progress +="*"
     # Бүх үсгийг нэг мөр болгон хэвлэнэ 
+    print("Одоогийн байдал: " + progress)
+   
+    
     pass
 
 def get_guess(already_guessed):
     # Хэрэглэгчээс input авна
+    gues = input("Та нэг үсэг таа: ").upper()
+
     # Оролт зөвхөн нэг үсэг эсэхийг шалгана
+    if len(gues) !=1 or not gues.isalpha():
+        print("Зөвхөн нэг үсэг оруулна уу.")
+        return get_guess(already_guessed)
+        
     # Аль хэдийн таасан үсэг бол дахин нэвтрүүлэх боломж олгоно
+    if gues in already_guessed:
+        print("Та энэ үсгийг аль хэдийн таасан байна. Өөр үсэг оруулна уу.")
+        return get_guess(already_guessed)
     # Зөв үсгийг буцаана
-    pass
+    return gues
 
 def update_state(word, guess, guessed_letters):
     # Жинхэнэ state-ийг шинэчилнэ
